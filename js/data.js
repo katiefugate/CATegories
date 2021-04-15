a095e0e49fe4e0b13b157ecdaea575e83b87fb17
katiefugate <katiiierochelle@yahoo.com>
2021-04-15 18:02:27 +0000

    added placeholder, added styling, added data model and localstorage.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,17 @@
 /* exported data */
+
+var data = {
+  favorites: []
+};
+
+var previousDataJSON = localStorage.getItem('cat-data');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function store(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('cat-data', dataJSON);
+}
+
+window.addEventListener('beforeunload', store);
